f8ca3c4fe76b8e2deec532c6b6644dbb0289e002
Insanitykid2879 <131005583+Insanitykid2879@users.noreply.github.com>
2024-08-21 12:56:37 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,25 @@
+#M1HW Main
+#Alex Alejandro
+#8/21/2024
+from functions.py import *
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ functions.py
@@ -0,0 +1,54 @@
+#M1HW Functions
+#Alex Alejandro
+#8/21/2024
+
+def menu():
+    print("Welcome to the Calculator Program")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Divide")
+    print("4. Multiply")
+    print("5. Exit")
+    input("Enter a number: ")
+
+
+def add():
+    
+
+
+
+def calc_add():
+
+
+def sub():
+    
+
+
+
+def calc_sub():
+
+
+
+
+def div():
+    
+
+
+
+def calc_div():
+
+
+
+
+
+def mul():
+    
+
+
+
+def calc_mul():
+
+
+
+
+
